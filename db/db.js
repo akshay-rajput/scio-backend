3fa548c642bfdf6d0393f2a2fb411beba913ba46
Akshay <akshayrajput102@gmail.com>
2021-07-17 23:34:26 +0530

    feat - db - setup connection to db

---
+++ db/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function connectDB(){
+    console.log('trying to connect to db...')
+    try{
+        await mongoose.connect(`mongodb://akshayrajput:${process.env.DB_PASS}@wisp01-shard-00-00.6sufq.mongodb.net:27017,wisp01-shard-00-01.6sufq.mongodb.net:27017,wisp01-shard-00-02.6sufq.mongodb.net:27017/QuizApp?ssl=true&replicaSet=atlas-zdx3kh-shard-0&authSource=admin&retryWrites=true&w=majority`, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+
+        console.log('Connection successful')
+    }
+    catch(error){
+        console.log('Error connecting to db: ', error)
+    }
+}
+
+module.exports = { connectDB }
